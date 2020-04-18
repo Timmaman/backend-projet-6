c50759435b865a01611e85f2aa2e2b0fb3e92320
Timothée Maman <maman.timothee@hotmail.fr>
2020-04-18 11:53:36 +0200

    corrections commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,32 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/user');
 
+const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 //Création d'un utilisateur
 exports.signup = (req, res, next) => {
-    const count = req.body.password.length;
-    if (count >= 8) {
-        bcrypt.hash(req.body.password, 10)
-            .then(hash => {
-                const user = new User({
-                    email: req.body.email,
-                    password: hash
-                });
-                user.save()
-                    .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                    .catch(error => res.status(400).json({ error }));
-            })
-            .catch(error => res.status(500).json({ error }));
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(password)
+    if (!email_regex.test(email)) {
+        return res.status(400).json({ message: 'Email invalide' });
     } else {
-        res.status(401).json({ message: 'Votre mot de passe doit au moins faire 8 caractères' });
+        const count = password.length;
+        if (count >= 8) {
+            bcrypt.hash(password, 10)
+                .then(hash => {
+                    const user = new User({
+                        email: email,
+                        password: hash
+                    });
+                    user.save()
+                        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                        .catch(error => res.status(400).json({ error }));
+                })
+                .catch(error => res.status(500).json({ error }));
+        } else {
+            res.status(401).json({ message: 'Votre mot de passe doit au moins faire 8 caractères' });
+        }
     }
 };
 
@@ -33,9 +40,10 @@ exports.login = (req, res, next) => {
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                        return res.status(401).json({ message: 'Mot de passe incorrect !' });
                     }
-                    res.status(200).json({
+                    res.status(201).json({
+                        message: 'Connexion réussie!',
                         userId: user._id,
                         token: jwt.sign({ userId: user._id },
                             'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }
